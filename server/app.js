ef2ad8820774d042da29e498dd29a52e8ea5e691
kanha-beep <kanhashree2223@gmail.com>
2025-11-17 20:21:32 +0530

    deploy

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ import express from "express"
 import path from "path";
 import AuthRoutes from "./routes/AuthRoutes.js"
 import BlogsRoutes from "./routes/BlogsRoutes.js"
+import CommentsRoutes from "./routes/CommentsRoutes.js"
 import { connectDB } from "./utils/db.js"
 const app = express()
 app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true, methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"] }))
@@ -17,12 +18,15 @@ app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 connectDB()
 app.use("/api/auth", AuthRoutes)
 app.use("/api/blogs", BlogsRoutes)
-app.use((error, req, res, next) => {
-    const { status = 500, message = "Something went wrong" } = error;
-    res.status(status).json({ message })
-})
+app.use("/api/blogs", CommentsRoutes)
+
 app.use((req, res, next) => {
   console.log(req.method, req.url);
   next();
 });
+
+app.use((error, req, res, next) => {
+    const { status = 500, message = "Something went wrong" } = error;
+    res.status(status).json({ message })
+})
 export default app
